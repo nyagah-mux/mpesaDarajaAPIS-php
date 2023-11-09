38f07a7970201f42cee618f6dfb6f3aa15887f6d
nyagah-mux <150344164+nyagah-mux@users.noreply.github.com>
2023-11-09 19:01:18 +0300

    comments addition
    To enhance readability and comprehension, detailed comments are provided.

--- stkPush.php
+++ stkPush.php
@@ -1,16 +1,26 @@
 <?php
-
+//setting correct time zone so that out timestamp is precise
 date_default_timezone_set("Africa/Nairobi");
+
+//for us to have the accessToken's scope, we include the accessToken generator code, accessToken.php
 include("accessToken.php");
+
+//initializint curl
 $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Authorization: Bearer '.$accessToken,
     'Content-Type: application/json'
 ]);
 curl_setopt($ch, CURLOPT_POST, 1);
+
+//from the test credentials
 $businessShortCode = 174379;
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+
+//generate real-time time
 $timeStamp = date("YmdHis");
+
+//compose the post's payload
 $curlPostLoad = [
     "BusinessShortCode" => 174379,
     "Password" => base64_encode($businessShortCode . $passkey . $timeStamp),
@@ -25,9 +35,14 @@ $curlPostLoad = [
     "TransactionDesc" => "software priced" 
 ];
 
+//encode post's payload into json
 $postLoadFinal = json_encode($curlPostLoad);
+
+//use the post body now
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postLoadFinal);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$response     = curl_exec($ch);
+$response  = curl_exec($ch);
 curl_close($ch);
+
+//at this time, an stk push is already made to the phoneNumber above and we the output the details
 echo $response;
